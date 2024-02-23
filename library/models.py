6cee8c80e7fc3d1070333ffa525069d21295bfeb
Eric <ericjesus403@gmail.com>
2024-02-23 16:04:05 -0300

    Criando model Category e Books

--- library/models.py
+++ library/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Nome', max_length=150)
+
+
+LANGUAGES = (
+    ('PT-BR', 'PORTUGUÊS(BRASIL)'),
+    ('EN-US', 'INGLÊS(ESTADOS UNIDOS)'),
+    ('ES', 'ESPANHOL'),
+    ('DE', 'ALEMÃO'),
+    ('EN-GB', 'INGLÊS(REINO UNIDO)'),
+    ('JA-JA', 'JAPONÊS'),
+    ('RU',  'RUSSO'),
+    ('ZH-TW', 'CHINÊS'),
+)
+
+
+class Book(models.Model):
+    name = models.CharField('Nome', max_length=255)
+    category = models.ForeignKey(
+        Category, verbose_name='Categoria', on_delete=models.PROTECT)
+    isbn = models.CharField('Número de identificação', max_length=255)
+    publication_year = models.DateField()
+    edition = models.CharField('Edição', max_length=4)
+    pages_number = models.CharField('Número de páginas', max_length=4)
+    synopsis = models.CharField('Sinopse', max_length=500)
+    language = models.CharField('Idioma',  choices=LANGUAGES, max_length=255)
+    quantity = models.PositiveIntegerField()
