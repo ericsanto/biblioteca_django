d2cec7606016504165d07a2d18f3f3b7359b586a
Eric <ericjesus403@gmail.com>
2024-02-23 18:20:53 -0300

    Criando view para o cadastro de usuários

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import UserForm
+from .models import UserCustom
 
-# Create your views here.
+
+class UserCreateView(CreateView):
+    model = UserCustom
+    form_class = UserForm
+    template_name = 'user_create.html'
+    success_url = reverse_lazy('home')
