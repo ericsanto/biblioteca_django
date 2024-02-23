fa7c5a776914c74afab659490e4292effc155ff6
Eric <ericjesus403@gmail.com>
2024-02-23 16:22:29 -0300

    Criando views para a criação de Category e  Book

--- library/views.py
+++ library/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Category, Book
+from .forms import CategoryForm, BookForm
 
-# Create your views here.
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category_create.html'
+    success_url = reverse_lazy('category_list_all')
+
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'book_create.html'
+    success_url = reverse_lazy('book_list_all')
