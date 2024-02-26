83dfd79443a203e648827cfd22fa0948c4f7e04f
Eric <ericjesus403@gmail.com>
2024-02-26 18:46:57 -0300

    Criando urls catalog, home, book_detail

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import CategoryCreateView, BookCreateView
-
+from .views import BookCreateView, BookDetailView, CategoryCreateView, CatalogBookListView, HomeTemplateView
 
 urlpatterns = [
+    path('inicio/', HomeTemplateView.as_view(), name='home'),
     path('cadastrar-categoria/', CategoryCreateView.as_view(),
          name='create_category'),
     path('cadastrar-livro/', BookCreateView.as_view(), name='create_book'),
+    path('catalogo/', CatalogBookListView.as_view(), name='catalog'),
+    path('detalhe-livro/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+
 ]
