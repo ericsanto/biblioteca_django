9e1c6a23366fa6629a0643cfef05206b4d6e3316
Eric <ericjesus403@gmail.com>
2024-02-26 08:50:36 -0300

    Incluindo urls do app user no projeto

--- app/urls.py
+++ app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
+    path('user/', include('user.urls')),
 ]
