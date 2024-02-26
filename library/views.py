20073bf94ad7c1fbefc1f8d0af9a75df2555250e
Eric <ericjesus403@gmail.com>
2024-02-26 18:45:11 -0300

    Criando views CatalogBookListView, HomeTemplateView, BookDetailView

--- library/views.py
+++ library/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, ListView, TemplateView
 from django.urls import reverse_lazy
 from .models import Category, Book
 from .forms import CategoryForm, BookForm
+
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+
+class CatalogBookListView(ListView):
+    model = Book
+    template_name = 'catalog_books.html'
+    context_object_name = 'books'
 
 
 class CategoryCreateView(CreateView):
@@ -17,3 +27,8 @@ class BookCreateView(CreateView):
     form_class = BookForm
     template_name = 'book_create.html'
     success_url = reverse_lazy('book_list_all')
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
