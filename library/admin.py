eac2aec82e930bac348cdfe7a43b82e2a3d2367d
Eric <ericjesus403@gmail.com>
2024-02-26 18:43:51 -0300

    Registrando model History no admin

--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Author, Book, Category
+from .models import Author, Book, Category, History
 
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Category)
+admin.site.register(History)
