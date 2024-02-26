1eda964e28f7821ff9f57efd459e7a60e8d726c8
Eric <ericjesus403@gmail.com>
2024-02-26 08:49:42 -0300

    Criando url para login de usuários

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import UserCreateView
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('cadastro-usuario/', UserCreateView.as_view(), name='user_create'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login')
 ]
