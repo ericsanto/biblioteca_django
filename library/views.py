0134620e71517d37e7b16bcb81d072435a987601
Eric <ericjesus403@gmail.com>
2024-02-27 19:04:55 -0300

    Adicionando filtros de pesquisas na view CatalogBookListView e adicionando  contexto na view HomeTemplateView

--- library/views.py
+++ library/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
 from django.urls import reverse_lazy
-from .models import Category, Book
-from .forms import CategoryForm, BookForm
+from .models import Book, Category, History
+from .forms import BookForm, CategoryForm
 
 
 class HomeTemplateView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['historys'] = History.objects.all()
+        return context
 
 
 class CatalogBookListView(ListView):
     model = Book
     template_name = 'catalog_books.html'
     context_object_name = 'books'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+    def get_queryset(self):
+        books = Book.objects.all()
+        search = self.request.GET.get('search')
+        category_url = self.request.GET.get('category')
+
+        if search:
+            books = books.filter(name__icontains=search)
+
+        if category_url:
+            books = books.filter(category__name=category_url)
+
+        return books
 
 
 class CategoryCreateView(CreateView):
