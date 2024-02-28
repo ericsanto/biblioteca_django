2f5a263711d9772b04db05eacc6e87acd725a6ca
Eric <ericjesus403@gmail.com>
2024-02-28 18:53:36 -0300

    Criando view para a reserva de livros

--- reserve/views.py
+++ reserve/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import ReserveBook
+from .forms import ReserveBookForm
+from library.models import Book
 
-# Create your views here.
+
+def reserve_book_create(request, book_id):
+    if request.method == 'POST':
+        book = Book.objects.get(id=book_id)
+        user = request.user
+        existing_reserve = ReserveBook.objects.filter(user=user).exists()
+        print(existing_reserve)
+        if not existing_reserve:
+            reserve = ReserveBook.objects.create(
+                user=user,
+                book=book,
+            )
+            reserve.save()
+            book.quantity -= 1
+            book.save()
+            return redirect('catalog')
+        else:
+            messages.error(request, 'Você já tem um livro agendado')
+            return redirect('catalog')
