2f4735bf5b10f01b49b88d7c4e86e8591d3c1fdc
Eric <ericjesus403@gmail.com>
2024-02-29 16:04:49 -0300

    Criando url para a view reserve_book_devolution e para a view ReserveBookListView

--- reserve/urls.py
+++ reserve/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import reserve_book_create
+from .views import reserve_book_create, reserve_book_devolution, ReserveBookListView
 
 urlpatterns = [
-    path('reserve-book/<int:book_id>/',
+    path('reservar-livro/<int:book_id>/',
          reserve_book_create, name='reserve_create'),
+    path('devolver-livro-reservado/<int:id_reserve>/',
+         reserve_book_devolution, name='reserve_devolution'),
+    path('livros-reservado/<int:pk>/',
+         ReserveBookListView.as_view(), name='book_reserve')
 ]
