a2ef2d106802c45c71344b3b881ba74f714a788e
Eric <ericjesus403@gmail.com>
2024-02-29 16:02:04 -0300

    Renomeando e adicionando fields: date_of_devolution -> devolution_deadline. Novo field: devolution

--- reserve/models.py
+++ reserve/models.py
@@ -9,19 +9,20 @@ class ReserveBook(models.Model):
         UserCustom, on_delete=models.PROTECT, verbose_name='Usuário')
     book = models.ForeignKey(
         Book, on_delete=models.PROTECT, verbose_name='Livro')
-    date_of_devolution = models.DateField(
-        'Data de devolução', blank=True, null=True)
+    devolution_deadline = models.DateField(
+        'Prazo de devolução', blank=True, null=True)
     date_of_reserve = models.DateField(
         verbose_name='Data da reserva', blank=True, null=True)
+    devolution = models.BooleanField('Devolvido', default=False)
 
     def __str__(self):
         return self.book.name
 
     def save(self, *args, **kwargs):
-        if not self.date_of_devolution:
+        if not self.devolution_deadline:
             date_actually = date.today()
             time_delta = timedelta(7)
-            self.date_of_devolution = date_actually + time_delta
+            self.devolution_deadline = date_actually + time_delta
 
         self.date_of_reserve = date.today()
         super().save(*args, **kwargs)
