287da80074d28380cbc3c3f037db5cb27110dd41
Eric <ericjesus403@gmail.com>
2024-03-04 16:17:19 -0300

    Criando views para a redefinição de senha: CustomPasswordResetView, CustomResetPasswordDone, CustomPasswordResetConfirmView

--- reset_password/views.py
+++ reset_password/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
+from .forms import FormPasswordResetView
 
-# Create your views here.
+
+class CustomPasswordResetView(PasswordResetView):
+    form_class = FormPasswordResetView
+    template_name = 'registration/password_reset.html'
+
+
+class CustomResetPasswordDone(PasswordResetDoneView):
+    template_name = 'registratiom/password_reset_done.html'
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'registration/password_reset_confirm.html'
