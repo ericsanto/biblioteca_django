44cc380850b9b3f512075ae44db0bf2d10a39ddc
Eric <ericjesus403@gmail.com>
2024-03-04 16:34:27 -0300

    Criando url para a view PasswordResetCompleteView

--- reset_password/urls.py
+++ reset_password/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import CustomPasswordResetView, CustomResetPasswordDone, CustomPasswordResetConfirmView
+from django.contrib.auth.views import PasswordResetCompleteView
+from .views import (
+    CustomPasswordResetView, 
+    CustomResetPasswordDone, 
+    CustomPasswordResetConfirmView,
+)
 
 
 urlpatterns = [
-    path('reset-password/', CustomPasswordResetView.as_view(), name='reset_password'),
+    path('reset-password/', CustomPasswordResetView.as_view(), 
+         name='reset_password'),
     path('reset-password-done/', CustomResetPasswordDone.as_view(),
          name='password_reset_done'),
-    path('reset-password-confirm/<slug:uidb64>/<slug:token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm')
+    path('reset-password-confirm/<slug:uidb64>/<slug:token>/', 
+         CustomPasswordResetConfirmView.as_view(), 
+         name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete_view')
+
 ]
