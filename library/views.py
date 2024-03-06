5f13fe9114a446abe956dd573567468a25bbdb3e
Eric <ericjesus403@gmail.com>
2024-03-06 16:42:24 -0300

    Criando view para o post de comentários

--- library/views.py
+++ library/views.py
@@ -1,7 +1,9 @@
+
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
 from django.urls import reverse_lazy
-from .models import Book, Category, History
-from .forms import BookForm, CategoryForm
+from django.shortcuts import redirect
+from .models import Book, Category, Comment, History
+from .forms import BookForm, CategoryForm, CommentBookForm
 from reserve.models import ReserveBook
 
 
@@ -66,5 +68,23 @@ class BookDetailView(DetailView):
         book = Book.objects.get(id=id_book)
         category_book = book.category
         context['category_books'] = Book.objects.filter(
-            category__name=category_book).exclude(id=id_book)
+            category__name=category_book).exclude(id=id_book)[:9]
+        context['comments'] = Comment.objects.all()
+        context['form'] = CommentBookForm()
+
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = CommentBookForm(request.POST)
+
+        if self.request.user.is_authenticated:
+            if form.is_valid():
+                book_id = self.kwargs['pk']
+                comment = form.cleaned_data['comment_user']
+                Comment.objects.create(comment_user=comment,
+                                       user=self.request.user, book_id=book_id)
+                return self.get(request, *args, **kwargs)
+            else:
+                return self.get(request, *args, **kwargs)
+        else:
+            return redirect('book_detail', self.kwargs['pk'])
