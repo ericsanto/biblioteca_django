70747806c408919b80fb8d93f6e62e3ecfc1468d
Eric <ericjesus403@gmail.com>
2024-03-07 12:06:02 -0300

    Criando view CommentCreateView  para a criação de comentários

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+from django.contrib import messages
+from library.models import Book
+from .models import Comment
+from .forms import CommentBookForm
 
-# Create your views here.
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentBookForm
+    template_name = 'comment_book.html'
+
+    def post(self, request, *args, **kwargs):
+        form = CommentBookForm(request.POST)
+        id_book = self.kwargs['pk']
+        book = Book.objects.get(id=id_book)
+        if request.method == 'POST':
+            if form.is_valid():
+                comment = form.cleaned_data['comment_user']
+                comment_created = Comment.objects.create(comment_user=comment, user=self.request.user,
+                                                         book=book)
+                messages.success(
+                    request, 'Comentário adicionado com sucesso! Obrigado por expor sua opinião')
+            return redirect('book_detail', pk=book.pk)
+        return super().get(request, *args, **kwargs)
