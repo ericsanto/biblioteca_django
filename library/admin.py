5c26f2137b15a36b55b7b4ce8722f5d2e1b666a2
Eric <ericjesus403@gmail.com>
2024-03-07 13:20:38 -0300

    Removendo model Comment do register

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Author, Book, Category, History
+from .models import Author, Book, Comment, History
 
 
 admin.site.register(Author)
 admin.site.register(Book)
-admin.site.register(Category)
 admin.site.register(History)
