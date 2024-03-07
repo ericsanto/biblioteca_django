9ae09ee682d6c958edde94d842ae9dde7c49eaf0
Eric <ericjesus403@gmail.com>
2024-03-07 12:07:30 -0300

    Criando url para a view CommentCreateView

--- comment/urls.py
+++ comment/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from .views import CommentCreateView
+
+
+urlpatterns = [
+    path('comentar-livro/<int:pk>/',
+         CommentCreateView.as_view(), name='comment_book'),
+]
