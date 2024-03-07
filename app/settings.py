bbe91404fff3d791ae46804d34be2dce5d6c8416
Eric <ericjesus403@gmail.com>
2024-03-07 15:39:05 -0300

    Instalando app comments

--- app/settings.py
+++ app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'reserve',
     'favorite',
     'bootstrap4',
+    'comment',
 ]
 
 MIDDLEWARE = [
@@ -111,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -140,3 +141,10 @@ LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ericjesus403@gmail.com'
+EMAIL_HOST_PASSWORD = 'tspxafhoiclguenw'
