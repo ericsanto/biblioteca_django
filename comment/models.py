5e68e32dab8a2493e973c8b9b5db4f2c405ec21c
Eric <ericjesus403@gmail.com>
2024-03-07 11:59:21 -0300

    Criando model Comment

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from library.models import Book
+from user.models import UserCustom
 
-# Create your models here.
+
+class Comment(models.Model):
+    comment_user = models.TextField()
+    user = models.ForeignKey(UserCustom, blank=True,
+                             null=True, on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book, blank=True, null=True, on_delete=models.CASCADE)
