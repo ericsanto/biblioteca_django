793cdebeeb6e9f117d09a91f4ac327d70c6c7acc
Eric <ericjesus403@gmail.com>
2024-03-11 16:41:26 -0300

    Adicionando contexto de view para livros com categorias específicas

--- library/views.py
+++ library/views.py
@@ -1,11 +1,14 @@
 from django.views.generic import CreateView, DetailView, ListView, TemplateView
-
+from django.conf import settings
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from comment.models import Comment
 from .forms import BookForm, CategoryForm
-from .models import Book, Category, History
+from .models import Author, Book, Category, History
 from reserve.models import ReserveBook
+import os
 
 
 class HomeTemplateView(TemplateView):
@@ -29,18 +32,26 @@ class CatalogBookListView(ListView):
         if self.request.user.is_authenticated:
             context['reserve'] = ReserveBook.objects.filter(
                 user=self.request.user)
+        context['authors'] = Author.objects.all()[:7]
+        context['books_informatics'] = Book.objects.filter(
+            category__name='Computação, Informática e Mídias Digitais')
+        context['books_literary'] =Book.objects.filter(category__name='Literatura')
         return context
 
     def get_queryset(self):
         books = Book.objects.all()
         search = self.request.GET.get('search')
         category_url = self.request.GET.get('category')
+        author_url = self.request.GET.get('author')
 
         if search:
             books = books.filter(name__icontains=search)
 
         if category_url:
             books = books.filter(category__name=category_url)
+
+        if author_url:
+            books = books.filter(author__name=author_url)
 
         return books
 
@@ -72,3 +83,12 @@ class BookDetailView(DetailView):
             category__name=category_book).exclude(id=id_book)[:9]
         context['comment'] = Comment.objects.filter(book_id=id_book)
         return context
+
+
+def download_pdf(request, book_id):
+    pdf_book = get_object_or_404(Book, id=book_id)
+    path_pdf = os.path.join(settings.MEDIA_ROOT, str(pdf_book.pdf_books))
+    with open(path_pdf, 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="{pdf_book.pdf_books.name}"'
+        return response
