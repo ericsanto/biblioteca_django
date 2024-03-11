883d103ef15fb0a353c5cff964c337cef965879f
Eric <ericjesus403@gmail.com>
2024-03-11 16:43:48 -0300

    Alteração

--- app/settings.py
+++ app/settings.py
@@ -142,9 +142,4 @@ LOGOUT_REDIRECT_URL = 'login'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ericjesus403@gmail.com'
-EMAIL_HOST_PASSWORD = 'tspxafhoiclguenw'
+
