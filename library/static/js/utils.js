a27bbff7d91c45083c4f142cf8e6875a8f12b56e
Eric <ericjesus403@gmail.com>
2024-03-20 11:51:36 -0300

    Criando pasta para arquivos javascript

---
+++ library/static/js/utils.js
@@ -0,0 +1,63 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  onClicked();
+})
+
+function onClicked(){
+  const click = document.getElementById('click')
+  console.log(click)
+}
+
+function alertReserve() {
+  if (document.body.textContent.includes('Parabéns, você agendou o livo')) {
+    Swal.fire({
+      title:"Parabéns",
+      text: "Livro agendado com Sucesso",
+      icon: "success",
+    });
+  } else if(document.body.textContent.includes('Você já tem um livro agendado'))
+    Swal.fire({
+      title: "Descuple",
+      text: "Você já tem um livro agendado",
+      icon: "error"
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  alertReserve(); 
+});
+
+function alertRenewReserve(){
+  if(document.body.textContent.includes('Livro renovado com sucesso')){
+    Swal.fire({
+      title: "Renovado",
+      text: "Livro renovado com sucesso",
+      icon: "success",
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  alertRenewReserve()
+});
+
+function alertReserveBookDevolution() {
+  if(document.body.textContent.includes('Livro devolvido com sucesso.')){
+    Swal.fire({
+      title: "Devolvido",
+      text: "Livro devolvido com sucesso",
+      icon: "success"
+    })
+  }else if(document.body.textContent.includes('Você não possui livro reservado')){
+    Swal.fire({
+      title: "Descuple",
+      text: "Você não possui nenhum livro agendado",
+      icon: "error",
+    })
+  } 
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  alertReserveBookDevolution()
+});
+
