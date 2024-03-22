22848274dc2342afe4f1439d6d4bd853b633a105
Eric <ericjesus403@gmail.com>
2024-03-22 09:55:52 -0300

    modificando url inicial

--- library/urls.py
+++ library/urls.py
@@ -10,11 +10,11 @@ from .views import (
 
 
 urlpatterns = [
-    path('', HomeTemplateView.as_view(), name='home'),
+    path('home/', HomeTemplateView.as_view(), name='home'),
     path('cadastrar-categoria/', CategoryCreateView.as_view(),
          name='create_category'),
     path('cadastrar-livro/', BookCreateView.as_view(), name='create_book'),
-    path('catalogo/', CatalogBookListView.as_view(), name='catalog'),
+    path('', CatalogBookListView.as_view(), name='catalog'),
     path('detalhe-livro/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('baixar-pdf/<book_id>/', download_pdf, name='download_pdf'),
 ]
